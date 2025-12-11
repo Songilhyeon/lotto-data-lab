be75191d994a28e4d12d211fc1eb9bc96c029092
SIH <lordangel82@gmail.com>
2025-12-11 19:24:27 +0900

    bizIfo추가

---
+++ src/app/constants/bizInfo.ts
@@ -0,0 +1,8 @@
+export const bizInfo = {
+  companyName: process.env.NEXT_PUBLIC_BIZ_NAME || "",
+  ownerName: process.env.NEXT_PUBLIC_BIZ_OWNER || "",
+  address: process.env.NEXT_PUBLIC_BIZ_ADDRESS || "",
+  businessNumber: process.env.NEXT_PUBLIC_BIZ_NUMBER || "",
+  contactEmail: process.env.NEXT_PUBLIC_BIZ_EMAIL || "",
+  tel: process.env.NEXT_PUBLIC_BIZ_TEL || "",
+};
