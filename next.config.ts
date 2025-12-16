c0afd05f840b1afe861d866eda2eb487afd2d57f
SIH <lordangel82@gmail.com>
2025-12-16 10:23:57 +0900

    사파리로그인문제수정중

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "production"
+            ? "https://lotto-data-lab.duckdns.org/api/:path*"
+            : "http://localhost:4000/api/:path*", // 로컬 Express 포트
+      },
+    ];
+  },
 };
 
 export default nextConfig;
