7a21bf93c4272d1e3cf0aa99f59a807e9477d6e6
SIH <lordangel82@gmail.com>
2025-12-16 16:15:21 +0900

    주의요함관련원인찾는중1

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "production"
-            ? "https://lotto-data-lab.duckdns.org/api/:path*"
-            : "http://localhost:4000/api/:path*", // 로컬 Express 포트
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
